a8ed89231381a383b48e9896d82b307ff5ae7c3f
benjmain <bdfulker2@hotmail.com>
2020-10-14 08:11:38 -0400

    Initial commit of FindDaysOfMonthTest project

---
+++ CSharpExcercises2/CSharpExercises2Tests/FindDaysOfMonthTests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace CSharpExercises2Tests
+{
+    [TestFixture]
+    public class FindDaysOfMonthTests
+    {
+        [Test]
+        public int DaysOfMonthTest(int month, int year)
+        {
+            return 1;
+        }
+    }
+}
