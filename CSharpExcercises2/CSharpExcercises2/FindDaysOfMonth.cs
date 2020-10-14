37e76f936091928c9d1e9e8a077ae0dd1dbd3623
benjmain <bdfulker2@hotmail.com>
2020-10-14 08:15:38 -0400

    added initial logic to FindDaysOfMonth.cs for method DaysOfMonth().

--- CSharpExcercises2/CSharpExcercises2/FindDaysOfMonth.cs
+++ CSharpExcercises2/CSharpExcercises2/FindDaysOfMonth.cs
@@ -4,5 +4,9 @@ namespace CSharpExcercises2
 {
     public class FindDaysOfMonth
     {
+        public int DaysOfMonth(int month, int year)
+        {
+            return DateTime.DaysInMonth(year, month);
+        }
     }
 }
