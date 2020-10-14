892bd27c67ae59875e4d89842acff1779a76ec0b
benjmain <bdfulker2@hotmail.com>
2020-10-14 08:23:22 -0400

    Added 14 more test cases

--- CSharpExcercises2/CSharpExercises2Tests/FindDaysOfMonthTests.cs
+++ CSharpExcercises2/CSharpExercises2Tests/FindDaysOfMonthTests.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using CSharpExcercises2;
 
 namespace CSharpExercises2Tests
 {
@@ -8,9 +9,24 @@ namespace CSharpExercises2Tests
     {
         [Test]
         [TestCase(2,2018, ExpectedResult = 28)]
+        [TestCase(1, 2018, ExpectedResult = 31)]
+        [TestCase(3, 2018, ExpectedResult = 31)]
+        [TestCase(4, 2018, ExpectedResult = 30)]
+        [TestCase(5, 2018, ExpectedResult = 31)]
+        [TestCase(6, 2018, ExpectedResult = 30)]
+        [TestCase(7, 2018, ExpectedResult = 31)]
+        [TestCase(8, 2018, ExpectedResult = 31)]
+        [TestCase(9, 2018, ExpectedResult = 30)]
+        [TestCase(10, 2018, ExpectedResult = 31)]
+        [TestCase(11, 2018, ExpectedResult = 30)]
+        [TestCase(12, 2018, ExpectedResult = 31)]
+        [TestCase(2, 2004, ExpectedResult = 29)]
+        [TestCase(2, 1800, ExpectedResult = 28)]
+        [TestCase(2, 1600, ExpectedResult = 29)]
         public int DaysOfMonthTest(int month, int year)
         {
-            return DaysOfMonth(month, year);
+            FindDaysOfMonth daysOfMonth = new FindDaysOfMonth();
+            return daysOfMonth.DaysOfMonth(month, year);
         }
     }
 }
