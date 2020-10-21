8f52c1cbf017c0b99a9d6d200fad33d5fd4edd11
benjmain <bdfulker2@hotmail.com>
2020-10-21 13:57:51 -0400

    Initial commit of test class for FindAbsoluteSumOfArrayOfIntegersTests.cs

---
+++ CSharpExcercises2/CSharpExercises2Tests/FindAbsoluteSumOfArrayOfIntegersTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CSharpExercises2Tests
+{
+    class FindAbsoluteSumOfArrayOfIntegersTests
+    {
+    }
+}
