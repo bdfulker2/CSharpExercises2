b09f19bd83309094dd1bad5c02e45b8af70cb0a0
benjmain <bdfulker2@hotmail.com>
2020-10-21 13:44:01 -0400

    Added logic for for DaysOfMonthNotUsingLIbrary() method

--- CSharpExcercises2/CSharpExcercises2/FindDaysOfMonth.cs
+++ CSharpExcercises2/CSharpExcercises2/FindDaysOfMonth.cs
@@ -8,5 +8,19 @@ namespace CSharpExcercises2
         {
             return DateTime.DaysInMonth(year, month);
         }
+
+        public int DaysOfMonthNotUsingLibrary(int month, int year)
+        {
+            if (year % 4 == 0 && month == 2)
+            {
+                if (year % 100 == 0 && year % 400 != 0) return 28;
+                else return 29;
+            }
+
+            if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) return 31;
+            else if (month == 2) return 28;
+            else if (month == 4 || month == 6 || month == 9 || month == 11) return 30;
+            else return 0;
+        }
     }
 }
