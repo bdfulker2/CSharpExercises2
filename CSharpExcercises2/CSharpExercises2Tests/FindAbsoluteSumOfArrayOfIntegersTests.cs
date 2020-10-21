174cfed4bc7e94baf53baf1f3b9c97b836b0ad5c
benjmain <bdfulker2@hotmail.com>
2020-10-21 15:04:30 -0400

    Added test for absolute sum using linq

--- CSharpExcercises2/CSharpExercises2Tests/FindAbsoluteSumOfArrayOfIntegersTests.cs
+++ CSharpExcercises2/CSharpExercises2Tests/FindAbsoluteSumOfArrayOfIntegersTests.cs
@@ -20,5 +20,17 @@ namespace CSharpExercises2Tests
             FindAbsoluteSumOfArrayOfIntegers absoluteSum = new FindAbsoluteSumOfArrayOfIntegers();
             return absoluteSum.GetAbsoluteSum(arrayOfInteger);
         }
+
+        [Test]
+        [TestCase(new int[] { 2, -1, -3, 4, 8 }, ExpectedResult = 18)]
+        [TestCase(new int[] { -1 }, ExpectedResult = 1)]
+        [TestCase(new int[] { -1, -3, -5, -4, -10, 0 }, ExpectedResult = 23)]
+        [TestCase(new int[] { 8, 9, 10, 32, 101, -10 }, ExpectedResult = 170)]
+        [TestCase(new int[] { 500 }, ExpectedResult = 500)]
+        public int GetAbsoluteSumWithLinq(int[] arrayOfInteger = null)
+        {
+            FindAbsoluteSumOfArrayOfIntegers absoluteSum = new FindAbsoluteSumOfArrayOfIntegers();
+            return absoluteSum.GetAbsoluteSum(arrayOfInteger);
+        }
     }
 }
