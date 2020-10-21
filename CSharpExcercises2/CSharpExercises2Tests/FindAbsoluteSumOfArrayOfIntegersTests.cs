6e035f1cfa5d8344bbc2f2e95fe722ad17933a07
benjmain <bdfulker2@hotmail.com>
2020-10-21 14:05:01 -0400

    Added Test Cases

--- CSharpExcercises2/CSharpExercises2Tests/FindAbsoluteSumOfArrayOfIntegersTests.cs
+++ CSharpExcercises2/CSharpExercises2Tests/FindAbsoluteSumOfArrayOfIntegersTests.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
+using CSharpExcercises2;
 
 namespace CSharpExercises2Tests
 {
-    class FindAbsoluteSumOfArrayOfIntegersTests
+    [TestFixture]
+    public class FindAbsoluteSumOfArrayOfIntegersTests
     {
+        [Test]
+        [TestCase(new int[] { 2, -1, -3, 4, 8 }, ExpectedResult = 18)]
+        [TestCase(new int[] { -1 }, ExpectedResult = 1)]
+        [TestCase(new int[] { -1, -3, -5, -4, -10, 0 }, ExpectedResult = 23)]
+        [TestCase(new int[] { 8, 9, 10, 32, 101, -10 }, ExpectedResult = 170)]
+        [TestCase(new int[] { 500 }, ExpectedResult = 500)]
+        public int GetAbsoluteSum(int[] arrayOfInteger = null)
+        {
+            return 0;
+        }
     }
 }
