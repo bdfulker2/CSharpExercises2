4df45a6ad3bd59b5be68adfe779af8fe765c6eda
benjmain <bdfulker2@hotmail.com>
2020-10-21 13:53:15 -0400

    Added Summary Comments

--- CSharpExcercises2/CSharpExcercises2/FindDaysOfMonth.cs
+++ CSharpExcercises2/CSharpExcercises2/FindDaysOfMonth.cs
@@ -2,6 +2,11 @@
 
 namespace CSharpExcercises2
 {
+    /// <summary>
+    /// Create a function that takes the month and year (as integers) and returns the number of days in that month.
+    /// Don't forget about leap years!
+    /// Although you can create the algorithm to determine the leap-year number for February, there is a library function that can really help.
+    /// </summary>
     public class FindDaysOfMonth
     {
         public int DaysOfMonth(int month, int year)
