5bf621193ef0445b00745b16fb6df0bbe0ef3b26
benjmain <bdfulker2@hotmail.com>
2020-10-21 13:56:20 -0400

    Initial Commit of FindAbsoluteSumOfArrayOfIntegers.cs

---
+++ CSharpExcercises2/CSharpExcercises2/FindAbsoluteSumOfArrayOfIntegers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpExcercises2
+{
+    /// <summary>
+    /// Take an array of integers (positive or negative or both) and return the sum of the absolute value of each element.
+    /// The term "absolute value" means to remove any negative sign in front of a number, and to think of all numbers as positive (or zero).
+    /// All the elements in the given array are integers.
+    /// </summary>
+    class FindAbsoluteSumOfArrayOfIntegers
+    {
+    }
+}
